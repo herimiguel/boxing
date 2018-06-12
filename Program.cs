f1abd0cdd4602dabf665534d1ec168a95f6c2a2e
Heriberto Miguel <herimiguel84@hotmail.com>
2018-06-12 09:35:10 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        List<object> box = new List<object>();
+        box.Add(7);
+        box.Add(28);
+        box.Add(-1);
+        box.Add(true);
+        box.Add("chair");
+
+        for(int i = 0; i<box.Count; i++){
+            Console.WriteLine(box[i]);
+        }
+            int sum = 0;
+            foreach (var x in box){
+                if(x is int){
+                    sum += (int)x;
+                }
+        }Console.WriteLine(value: "The sum of the box is" + sum);
+        }
+    }
+}
